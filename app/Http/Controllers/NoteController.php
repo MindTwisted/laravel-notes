c8316808cd4477cefe9c542402ebab7823f7d1d8
Mind Twisted <nikolaynikm@gmail.com>
2018-04-30 17:53:12 +0300

    NoteController destroy method add;

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -99,6 +99,12 @@ class NoteController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $note = Note::find($id);
+        $note->delete();
+
+        return response()->json([
+            'message' =>
+                "Note with id {$id} was successfully deleted",
+        ]);
     }
 }
